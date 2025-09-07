6426d6e3d86bb4471d78982cb8fbbc24434aa81b
MbonuJennifer <MbonuifeomaJennifer@gmail.com>
2025-09-07 23:11:44 +0100

    Created cta and why me sections

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { AboutComponent } from '../../components/about/about.component';
+import { CtaComponent } from '../../components/cta/cta.component';
+import { WhyMeComponent } from "../../components/why-me/why-me.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, AboutComponent],
+  imports: [HeroComponent, AboutComponent, CtaComponent, WhyMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
